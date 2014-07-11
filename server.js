d38395a13f639a3c98da9c94afc5f1c905ba4665
Justin Kalweit <kalweit@alumni.duke.edu>
2014-07-11 18:06:37 -0400

    Start development of nodejs server.

---
+++ server.js
@@ -0,0 +1,22 @@
+var port = process.env.PORT || 1337;
+
+var fs = require('fs');
+var http = require('http');
+var server = http.createServer(function (req, res) {
+
+    var file = req.url;
+    if(file == '/') file = '/index.html';
+
+    fs.readFile('.' + file, function (err, data) {
+
+        if(err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+
+        res.writeHead(200);
+        res.end(data);
+    });
+
+}).listen(port);
